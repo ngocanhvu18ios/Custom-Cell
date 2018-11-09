f646323c0fac54362279537a8ee62709722ba5d3
Admin <ngocanhvu18ict@gmail.com>
2018-11-09 12:51:06 +0700

    init

---
+++ Custom Cell/StoriesTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  StoriesTableViewController.swift
+//  Custom Cell
+//
+//  Created by Admin on 11/9/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+struct Headline {
+    
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+
+}
+class StoriesTableViewController: UITableViewController {
+    var headlines = [
+        Headline(id: 1, title: "Lorem Ipsum", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", image: "Apple"),
+        Headline(id: 2, title: "Aenean condimentum", text: "Ut eget massa erat. Morbi mauris diam, vulputate at luctus non.", image: "Banana"),
+        Headline(id: 3, title: "In ac ante sapien", text: "Aliquam egestas ultricies dapibus. Nam molestie nunc.", image: "Cantaloupe"),
+        ]
+    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return headlines.count
+    }
+    
+  
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        let headline = headlines[indexPath.row]
+        cell.textLabel?.text = headline.title
+        cell.detailTextLabel?.text = headline.text
+        cell.imageView?.image = UIImage(named: headline.image)
+        
+        return cell
+    }
+    
+}
